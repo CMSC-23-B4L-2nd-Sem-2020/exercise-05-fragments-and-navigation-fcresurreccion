0f6d2c9aa09c93eee15ae738cafc9c50c53102aa
fcresurreccion <fcresurreccion@up.edu.ph>
2020-03-13 11:52:18 +0800

    feat: create StartFragment class and layout file

---
+++ app/src/main/java/com/example/resurreccion_exer5_lightsoutv2/StartFragment.kt
@@ -0,0 +1,28 @@
+package com.example.resurreccion_exer5_lightsoutv2
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.resurreccion_exer5_lightsoutv2.databinding.FragmentStartBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StartFragment : Fragment() {
+
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_start, container, false)
+//    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentStartBinding>(inflater,
+            R.layout.fragment_start,container,false)
+        return binding.root
+    }
+}

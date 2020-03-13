bf9292bb8e2f0cf34b3fce7cd0c797a28e4e9ee1
fcresurreccion <fcresurreccion@up.edu.ph>
2020-03-13 11:15:05 +0800

    feat: add binding to onCreate

--- app/src/main/java/com/example/resurreccion_exer5_lightsoutv2/MainActivity.kt
+++ app/src/main/java/com/example/resurreccion_exer5_lightsoutv2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.resurreccion_exer5_lightsoutv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.resurreccion_exer5_lightsoutv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
